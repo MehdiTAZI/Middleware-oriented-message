b6cf637ae731bf357f7e3771a079b282dfa4aa0c
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2010-12-12 23:50:05 +0000

    Essai de EasyMock sur le test unitaire de Add Event en mockant le DAO et le ProxyPushSupplier

--- trunk/mezzoproto-parent/mezzoproto-eventserver/src/main/java/fr/esiag/mezzodijava/mezzoproto/eventserver/model/EventString.java
+++ trunk/mezzoproto-parent/mezzoproto-eventserver/src/main/java/fr/esiag/mezzodijava/mezzoproto/eventserver/model/EventString.java
@@ -6,31 +6,43 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name="EventString")
-public class EventString{
+@Entity(name = "EventString")
+public class EventString {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	@Column(name="Event")
+
+	@Column(name = "Event")
 	private String event;
-	
+
 	public EventString(String event) {
-		this.event=event;
+		this.event = event;
 	}
 
 	public String message() {
-		
+
 		return event;
 	}
 
 	public void message(String event) {
-		this.event=event;
-		
+		this.event = event;
+
 	}
 
-	
-	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof EventString){
+			return (((EventString)obj).id == this.id);
+			
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
 }
