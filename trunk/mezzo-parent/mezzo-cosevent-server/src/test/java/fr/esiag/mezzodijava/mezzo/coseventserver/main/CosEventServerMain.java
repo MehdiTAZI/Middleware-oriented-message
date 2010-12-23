dc2e3ca13155dab318272edd870140b54a0d505c
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2010-12-23 14:26:40 +0000

    Projets Mezzo

---
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/test/java/fr/esiag/mezzodijava/mezzo/coseventserver/main/CosEventServerMain.java
@@ -0,0 +1,17 @@
+package fr.esiag.mezzodijava.mezzo.coseventserver.main;
+
+public class CosEventServerMain {
+
+	public CosEventServerMain() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Mezzo di Java COS Event Server");
+
+	}
+
+}
