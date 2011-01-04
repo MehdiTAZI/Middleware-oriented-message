46ee0368eb030eefb4a8c8795ae9edfc934ccd9e
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-01-04 15:17:07 +0000

    coseventserver : le ProxyForPushConsumerImpl

---
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/impl/ProxyForPushConsumerImpl.java
@@ -0,0 +1,54 @@
+package fr.esiag.mezzodijava.mezzo.coseventserver.impl;
+
+import fr.esiag.mezzodijava.mezzo.cosevent.AlreadyConnectedException;
+import fr.esiag.mezzodijava.mezzo.cosevent.AlreadyRegisteredException;
+import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumer;
+import fr.esiag.mezzodijava.mezzo.cosevent.NotConnectedException;
+import fr.esiag.mezzodijava.mezzo.cosevent.NotRegisteredException;
+import fr.esiag.mezzodijava.mezzo.cosevent.ProxyForPushConsumerOperations;
+import fr.esiag.mezzodijava.mezzo.coseventserver.ctr.ChannelCtr;
+
+public class ProxyForPushConsumerImpl implements ProxyForPushConsumerOperations {
+
+	/**
+	 * The Channel Controller used by this facade
+	 */
+	ChannelCtr channelCtr;
+
+	CallbackConsumer callbackConsumer;
+
+	/**
+	 * Build an instance of ProxyForPushConsumerImpl associated to a Channel
+	 * Controler
+	 * 
+	 * @param channelCtr
+	 *            A Channel Controller
+	 */
+	public ProxyForPushConsumerImpl(ChannelCtr channelCtr) {
+		this.channelCtr = channelCtr;
+	}
+
+	@Override
+	public void subscribe(CallbackConsumer cc)
+			throws AlreadyRegisteredException {
+		this.callbackConsumer = cc;
+		channelCtr.addCallbackConsumer(cc);
+	}
+
+	@Override
+	public void unsubscribe() throws NotRegisteredException {
+		channelCtr.removeCallbackConsumer(this.callbackConsumer);
+	}
+
+	@Override
+	public void connect() throws AlreadyConnectedException {
+		channelCtr.addProxyForPushConsumer(this);
+	}
+
+	@Override
+	public void disconnect() throws NotConnectedException {
+		channelCtr.removeProxyForPushConsumer(this);
+
+	}
+
+}
