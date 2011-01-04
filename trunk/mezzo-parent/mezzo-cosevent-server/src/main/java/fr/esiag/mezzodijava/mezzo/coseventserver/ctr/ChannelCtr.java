bb63a3e83cc989fa2f6f5f5f8433f5540eb3be75
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-01-04 15:00:19 +0000

    coseventserver : le channelctr

---
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/ctr/ChannelCtr.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package fr.esiag.mezzodijava.mezzo.coseventserver.ctr;
+
+import java.nio.channels.AlreadyConnectedException;
+import java.util.HashSet;
+import java.util.Set;
+
+import fr.esiag.mezzodijava.mezzo.cosevent.AlreadyRegisteredException;
+import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumer;
+import fr.esiag.mezzodijava.mezzo.cosevent.Event;
+import fr.esiag.mezzodijava.mezzo.cosevent.NotConnectedException;
+import fr.esiag.mezzodijava.mezzo.cosevent.NotRegisteredException;
+import fr.esiag.mezzodijava.mezzo.cosevent.ProxyForPushConsumerOperations;
+import fr.esiag.mezzodijava.mezzo.cosevent.ProxyForPushSupplierOperations;
+import fr.esiag.mezzodijava.mezzo.coseventserver.model.Channel;
+
+/**
+ * @author Franck
+ * 
+ */
+public class ChannelCtr {
+
+	// lien vers le model
+	Channel channel;
+
+	Set<CallbackConsumer> callbackConsumers = new HashSet<CallbackConsumer>();
+
+	Set<ProxyForPushConsumerOperations> proxyForPushConsumers = new HashSet<ProxyForPushConsumerOperations>();
+
+	Set<ProxyForPushSupplierOperations> proxyForPushSuppliers = new HashSet<ProxyForPushSupplierOperations>();
+
+	/**
+	 * Build instance of a ChannelCtr associated with a Channel entity
+	 * 
+	 * @param channel
+	 *            Channel entity
+	 */
+	public ChannelCtr(Channel channel) {
+		this.channel = channel;
+	}
+
+	public void addCallbackConsumer(CallbackConsumer callbackConsummer)
+			throws AlreadyRegisteredException {
+		if (!callbackConsumers.add(callbackConsummer)) {
+			throw new AlreadyRegisteredException();
+		}
+	}
+
+	public void removeCallbackConsumer(CallbackConsumer callbackConsummer)
+			throws NotRegisteredException {
+		if (!callbackConsumers.remove(callbackConsummer)) {
+			throw new NotRegisteredException();
+		}
+	}
+
+	public void addProxyForPushConsumer(
+			ProxyForPushConsumerOperations proxyConsumer) {
+		if (!proxyForPushConsumers.add(proxyConsumer)) {
+			throw new AlreadyConnectedException();
+		}
+	}
+
+	public void removeProxyForPushConsumer(
+			ProxyForPushConsumerOperations proxyConsumer)
+			throws NotConnectedException {
+		if (!proxyForPushConsumers.remove(proxyConsumer)) {
+			throw new NotConnectedException();
+		}
+	}
+
+	public void addProxyForPushSupplier(
+			ProxyForPushSupplierOperations proxySupplier) {
+		if (!proxyForPushSuppliers.add(proxySupplier)) {
+			throw new AlreadyConnectedException();
+		}
+	}
+
+	public void removeProxyForPushSupplier(
+			ProxyForPushSupplierOperations proxySupplier)
+			throws NotConnectedException {
+		if (!proxyForPushSuppliers.remove(proxySupplier)) {
+			throw new NotConnectedException();
+		}
+	}
+
+	public void addEvent(Event e) {
+		channel.addEvents(e);
+	}
+
+}
