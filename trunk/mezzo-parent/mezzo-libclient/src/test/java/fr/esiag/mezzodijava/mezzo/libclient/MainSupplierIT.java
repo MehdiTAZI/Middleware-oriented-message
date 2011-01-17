78d57e2110419da49e412c797b204cb818d9222a
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-01-17 10:12:05 +0000

    idl : event content devient string et timestamp passe en long long

--- trunk/mezzo-parent/mezzo-libclient/src/test/java/fr/esiag/mezzodijava/mezzo/libclient/MainSupplierIT.java
+++ trunk/mezzo-parent/mezzo-libclient/src/test/java/fr/esiag/mezzodijava/mezzo/libclient/MainSupplierIT.java
@@ -1,5 +1,7 @@
 package fr.esiag.mezzodijava.mezzo.libclient;
-import org.omg.CORBA.Any;
+
+import java.util.Date;
+
 import org.omg.CORBA.ORB;
 
 import fr.esiag.mezzodijava.mezzo.cosevent.AlreadyConnectedException;
@@ -12,41 +14,45 @@ import fr.esiag.mezzodijava.mezzo.cosevent.ProxyForPushSupplier;
 import fr.esiag.mezzodijava.mezzo.libclient.exception.EventClientException;
 import fr.esiag.mezzodijava.mezzo.libclient.exception.TopicNotFoundException;
 
-
 public class MainSupplierIT {
 
 	/**
 	 * @param args
-	 * @throws EventClientException 
-	 * @throws TopicNotFoundException 
-	 * @throws ChannelNotFoundException 
-	 * @throws AlreadyConnectedException 
-	 * @throws MaximalConnectionReachedException 
+	 * @throws EventClientException
+	 * @throws TopicNotFoundException
+	 * @throws ChannelNotFoundException
+	 * @throws AlreadyConnectedException
+	 * @throws MaximalConnectionReachedException
 	 */
 	private ORB orb;
-	public MainSupplierIT() throws EventClientException, TopicNotFoundException, ChannelNotFoundException, MaximalConnectionReachedException, AlreadyConnectedException{
+
+	public MainSupplierIT() throws EventClientException,
+			TopicNotFoundException, ChannelNotFoundException,
+			MaximalConnectionReachedException, AlreadyConnectedException {
 		EventClient ec = EventClient.init(null);
-		orb=ec.getOrb();
-		ChannelAdmin channelAdmin= ec.resolveChannelByTopic("MEZZO");
-		
-		ProxyForPushSupplier supplierProxy = channelAdmin.getProxyForPushSupplier();			
+		orb = ec.getOrb();
+		ChannelAdmin channelAdmin = ec.resolveChannelByTopic("MEZZO");
+
+		ProxyForPushSupplier supplierProxy = channelAdmin
+				.getProxyForPushSupplier();
 		supplierProxy.connect();
-		Any any=orb.create_any();
-		any.insert_string("TEST");
-		for(int i=0;i<3;i++)
+		for (int i = 0; i < 3; i++)
 			try {
-				supplierProxy.push(new Event(i, any));
+				supplierProxy
+						.push(new Event((new Date()).getTime(), "TEST" + i));
 			} catch (NotConnectedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
+
 		System.out.println("ALL DONE");
-		
+
 	}
-	
-	public static void main(String[] args) throws ChannelNotFoundException, MaximalConnectionReachedException, AlreadyConnectedException, EventClientException, TopicNotFoundException {
-			new MainSupplierIT();
+
+	public static void main(String[] args) throws ChannelNotFoundException,
+			MaximalConnectionReachedException, AlreadyConnectedException,
+			EventClientException, TopicNotFoundException {
+		new MainSupplierIT();
 
 	}
 
--- trunk/mezzo-parent/mezzo-libclient/src/test/java/fr/esiag/mezzodijava/mezzo/libclient/callBackConsumerImpl.java
+++ trunk/mezzo-parent/mezzo-libclient/src/test/java/fr/esiag/mezzodijava/mezzo/libclient/callBackConsumerImpl.java
@@ -1,17 +1,5 @@
 package fr.esiag.mezzodijava.mezzo.libclient;
 
-import org.omg.CORBA.Context;
-import org.omg.CORBA.ContextList;
-import org.omg.CORBA.DomainManager;
-import org.omg.CORBA.ExceptionList;
-import org.omg.CORBA.NVList;
-import org.omg.CORBA.NamedValue;
-import org.omg.CORBA.Object;
-import org.omg.CORBA.Policy;
-import org.omg.CORBA.Request;
-import org.omg.CORBA.SetOverrideType;
-
-import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumer;
 import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumerOperations;
 import fr.esiag.mezzodijava.mezzo.cosevent.ConsumerNotFoundException;
 import fr.esiag.mezzodijava.mezzo.cosevent.Event;
@@ -21,7 +9,8 @@ public class callBackConsumerImpl implements CallbackConsumerOperations {
 	@Override
 	public void receive(Event evt) throws ConsumerNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("Message recu :" +evt);
+		System.out.println("Message recu timestamp:" + evt.timestamp
+				+ ", contenu" + evt.content);
 	}
 
 }
