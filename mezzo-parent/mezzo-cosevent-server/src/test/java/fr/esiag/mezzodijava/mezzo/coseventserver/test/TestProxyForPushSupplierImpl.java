1a4f766908ca13ab21cea95691a17eb3411dce0e
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-01-17 23:42:02 +0000

    test server: tout dans un package de test

--- mezzo-parent/mezzo-cosevent-server/src/test/java/fr/esiag/mezzodijava/mezzo/coseventserver/test/TestProxyForPushSupplierImpl.java
+++ mezzo-parent/mezzo-cosevent-server/src/test/java/fr/esiag/mezzodijava/mezzo/coseventserver/test/TestProxyForPushSupplierImpl.java
@@ -1,4 +1,4 @@
-package fr.esiag.mezzodijava.mezzo.coseventserver.main;
+package fr.esiag.mezzodijava.mezzo.coseventserver.test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
