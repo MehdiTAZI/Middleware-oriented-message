e7939a116a05acf11a593e30093ea5add11cd5ac
tazihosniomar <tazihosniomar@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-16 17:39:48 +0000

    MEZZO

--- trunk/mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/main/CosTimeServer.java
+++ trunk/mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/main/CosTimeServer.java
@@ -1,20 +1,38 @@
 package fr.esiag.mezzodijava.mezzo.costimeserver.main;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.omg.CORBA.ORB;
+
+import fr.esiag.mezzodijava.mezzo.costimeserver.ctr.TimeServiceCtr;
+import fr.esiag.mezzodijava.mezzo.costimeserver.impl.TimeServiceImpl;
+import fr.esiag.mezzodijava.mezzo.costimeserver.model.TimeServiceModel;
 import fr.esiag.mezzodijava.mezzo.costimeserver.publisher.TimeServicePublisher;
 
 public class CosTimeServer {
 
-	public CosTimeServer() {
-		// TODO Auto-generated constructor stub
+	public CosTimeServer(String[] args) {
+		Properties props = new Properties();
+		try {
+		    props.load(this.getClass().getClassLoader()
+			    .getResourceAsStream("eventserver.properties"));
+		} catch (IOException e) {
+		    // TODO log here
+		    
+		}
+		TimeServicePublisher publisher=new TimeServicePublisher();
+		TimeServiceCtr ctr=new TimeServiceCtr(new TimeServiceModel());
+		TimeServiceImpl timeService=new TimeServiceImpl(ctr);
+		ORB orb = ORB.init(args, props);
+		publisher.publish("cosTime", timeService, orb);
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		TimeServicePublisher publisher=new TimeServicePublisher();
-		
-		//publisher.publish("cosTime", timeService, orb)
+		new CosTimeServer(args);
 	}
 
 }
