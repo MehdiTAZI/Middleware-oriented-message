a6f7bea7690fbff8afadda5930014cb00f418d52
tazihosniomar <tazihosniomar@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-16 17:56:48 +0000

    MEZZO

---
+++ mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/main/Test.java
@@ -0,0 +1,50 @@
+package fr.esiag.mezzodijava.mezzo.costimeserver.main;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.Object;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import fr.esiag.mezzodijava.mezzo.costime.TimeService;
+import fr.esiag.mezzodijava.mezzo.costime.TimeServiceHelper;
+import fr.esiag.mezzodijava.mezzo.costime.TimeServicePOATie;
+import fr.esiag.mezzodijava.mezzo.costime._TimeServiceStub;
+
+public class Test {
+
+	public Test(String [] args){
+		Properties props = new Properties();
+		try {
+		    props.load(this.getClass().getClassLoader()
+			    .getResourceAsStream("eventserver.properties"));
+		} catch (IOException e) {
+		    // TODO log here
+		    
+		}
+		try{
+		ORB orb=ORB.init(args,props);
+		POA rootPOA=POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+		rootPOA.the_POAManager().activate();
+		Object objRef= orb.resolve_initial_references("NameService");
+		NamingContextExt ncRef =NamingContextExtHelper.narrow(objRef);
+		TimeService service=TimeServiceHelper.narrow(ncRef.resolve_str("cosTime"));
+		TimeServicePOATie tie=new TimeServicePOATie(new Component());
+		service.subscribe(tie._this(orb));
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Test(args);
+	}
+
+}
