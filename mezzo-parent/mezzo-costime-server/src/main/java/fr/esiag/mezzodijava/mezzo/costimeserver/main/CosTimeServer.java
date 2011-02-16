641202d150183d57cc26a5c7d0263ba119bff603
tazihosniomar <tazihosniomar@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-16 17:31:54 +0000

    MEZZO

--- mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/main/CosTimeServer.java
+++ mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/main/CosTimeServer.java
@@ -1,5 +1,7 @@
 package fr.esiag.mezzodijava.mezzo.costimeserver.main;
 
+import fr.esiag.mezzodijava.mezzo.costimeserver.publisher.TimeServicePublisher;
+
 public class CosTimeServer {
 
 	public CosTimeServer() {
@@ -10,7 +12,9 @@ public class CosTimeServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		TimeServicePublisher publisher=new TimeServicePublisher();
 		
+		//publisher.publish("cosTime", timeService, orb)
 	}
 
 }
