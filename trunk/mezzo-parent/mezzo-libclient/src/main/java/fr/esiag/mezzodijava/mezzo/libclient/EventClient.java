44783b538173244938661549c0d8e999aeb01ccf
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-27 23:19:45 +0000

    test COSEventIT : 1s-1c 1s-10c 10s-10c

--- trunk/mezzo-parent/mezzo-libclient/src/main/java/fr/esiag/mezzodijava/mezzo/libclient/EventClient.java
+++ trunk/mezzo-parent/mezzo-libclient/src/main/java/fr/esiag/mezzodijava/mezzo/libclient/EventClient.java
@@ -14,6 +14,8 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumer;
 import fr.esiag.mezzodijava.mezzo.cosevent.CallbackConsumerOperations;
@@ -51,189 +53,191 @@ import fr.esiag.mezzodijava.mezzo.libclient.exception.TopicNotFoundException;
  */
 public final class EventClient {
 
-	/**
-	 * Default Event Client Property File.
-	 */
-	private final static String CLIENT_PROPERTIES = "eventclient.properties";
+    private static Logger log = LoggerFactory.getLogger(EventClient.class);
+    /**
+     * Default Event Client Property File.
+     */
+    private final static String CLIENT_PROPERTIES = "eventclient.properties";
 
-	private static EventClient instance;
+    private static EventClient instance;
 
-	/**
-	 * Give a singleton instance of EventClient with his orb and his nameservice
-	 * initialized.
-	 * 
-	 * @param args
-	 *            Command line parameters
-	 * @return a initialized singleton instance of EventClient
-	 * @throws EventClientException
-	 *             Cannot init a resource (ORB or NameService) with this
-	 *             configuration
-	 */
-	public static synchronized EventClient init(String[] args)
-	throws EventClientException {
-		if (instance == null) {
-			// // Properties à externaliser
-			// props = new Properties();
-			// props.setProperty("ORBInitRef.NameService",
-			// "corbaloc::127.0.0.1:1050/NameService");
-			// props.setProperty("jacorb.home", System.getenv("JACORB_HOME"));
-			// props.setProperty("org.omg.CORBA.ORBClass",
-			// "org.jacorb.orb.ORB");
-			// props.setProperty("org.omg.CORBA.ORBSingletonClass",
-			// "org.jacorb.orb.ORBSingleton");
-			// // props.setProperty("java.endorsed.dirs",
-			// // System.getenv("JACORB_HOME") + "/lib");
-			String[] cmdArgs = args == null ? null : Arrays.copyOf(args,
-					args.length);
-			instance = new EventClient(cmdArgs, null);
-		}
-		return instance;
+    /**
+     * Give a singleton instance of EventClient with his orb and his nameservice
+     * initialized.
+     * 
+     * @param args
+     *            Command line parameters
+     * @return a initialized singleton instance of EventClient
+     * @throws EventClientException
+     *             Cannot init a resource (ORB or NameService) with this
+     *             configuration
+     */
+    public static synchronized EventClient init(String[] args)
+	    throws EventClientException {
+	if (instance == null) {
+	    // // Properties à externaliser
+	    // props = new Properties();
+	    // props.setProperty("ORBInitRef.NameService",
+	    // "corbaloc::127.0.0.1:1050/NameService");
+	    // props.setProperty("jacorb.home", System.getenv("JACORB_HOME"));
+	    // props.setProperty("org.omg.CORBA.ORBClass",
+	    // "org.jacorb.orb.ORB");
+	    // props.setProperty("org.omg.CORBA.ORBSingletonClass",
+	    // "org.jacorb.orb.ORBSingleton");
+	    // // props.setProperty("java.endorsed.dirs",
+	    // // System.getenv("JACORB_HOME") + "/lib");
+	    String[] cmdArgs = args == null ? null : Arrays.copyOf(args,
+		    args.length);
+	    instance = new EventClient(cmdArgs, null);
 	}
+	return instance;
+    }
 
-	private POA callbacksPOA;
+    private POA callbacksPOA;
 
-	private NamingContextExt nce;
+    private NamingContextExt nce;
 
-	private ORB orb;
+    private ORB orb;
 
-	/**
-	 * ORB Properties
-	 */
-	private Properties props;
+    /**
+     * ORB Properties
+     */
+    private Properties props;
 
-	//
-	// private String[] args = { "-ORBInitRef",
-	// "NameService=corbaloc::127.0.0.1:1050/NameService",
-	// "-Djacorb.home=C:\\mezzodev\\jacorb-2.3.1",
-	// "-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB",
-	// "-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton" };
+    //
+    // private String[] args = { "-ORBInitRef",
+    // "NameService=corbaloc::127.0.0.1:1050/NameService",
+    // "-Djacorb.home=C:\\mezzodev\\jacorb-2.3.1",
+    // "-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB",
+    // "-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton" };
 
-	/**
-	 * Event Client private Constructor. Use EventClient.init
-	 * 
-	 * @param args
-	 *            Program Arguments
-	 * @param properties
-	 *            optional properties
-	 * @throws EventClientException
-	 *             Initialization failure
-	 */
-	private EventClient(String[] args, Properties properties)
-	throws EventClientException {
-		props = properties;
-		if (props == null) {
-			props = new Properties();
-			try {
-				props.load(this.getClass().getClassLoader()
-						.getResourceAsStream(EventClient.CLIENT_PROPERTIES));
-			} catch (IOException e) {
-				// TODO log here
-				throw new EventClientException(
-						"Error in opening client property file", e);
-			}
-		}
-		orb = ORB.init(args, props);
-		Object nceObj = null;
-		try {
-			nceObj = orb.resolve_initial_references("NameService");
-		} catch (InvalidName e) {
-			// TODO log here
-			throw new EventClientException("Cannot resolve NameService", e);
-		}
-		nce = NamingContextExtHelper.narrow(nceObj);
+    /**
+     * Event Client private Constructor. Use EventClient.init
+     * 
+     * @param args
+     *            Program Arguments
+     * @param properties
+     *            optional properties
+     * @throws EventClientException
+     *             Initialization failure
+     */
+    private EventClient(String[] args, Properties properties)
+	    throws EventClientException {
+	System.out.println("Initilazing Mezzo Client...");
+	props = properties;
+	if (props == null) {
+	    props = new Properties();
+	    try {
+		props.load(this.getClass().getClassLoader()
+			.getResourceAsStream(EventClient.CLIENT_PROPERTIES));
+	    } catch (IOException e) {
+		// TODO log here
+		throw new EventClientException(
+			"Error in opening client property file", e);
+	    }
 	}
-
-	public ORB getOrb() {
-		return orb;
+	orb = ORB.init(args, props);
+	Object nceObj = null;
+	try {
+	    nceObj = orb.resolve_initial_references("NameService");
+	} catch (InvalidName e) {
+	    // TODO log here
+	    throw new EventClientException("Cannot resolve NameService", e);
 	}
+	nce = NamingContextExtHelper.narrow(nceObj);
+	System.out.println("Mezzo Client initialized.");
+    }
 
-	/**
-	 * Resolve a Channel with its topic using the NameService declared in
-	 * configuration.
-	 * 
-	 * @param topic
-	 *            Channel to find topic
-	 * @return an instance of Channel (distant object)
-	 * @throws EventClientException
-	 *             when name resolution is wrong
-	 * @throws TopicNotFoundException
-	 */
-	public ChannelAdmin resolveChannelByTopic(String topic)
-	throws EventClientException, TopicNotFoundException {
-		Object channelObj = null;
-		try {
-			channelObj = nce.resolve_str(topic);
-			// TODO Design a fail-over to switch between several NameService
-			// before throwing exception
-		} catch (NotFound e) {
-			// TODO log here
-			throw new TopicNotFoundException("Cannot find the Topic '" + topic
-					+ "'", e);
-		} catch (CannotProceed e) {
-			// TODO log here
-			throw new EventClientException("Cannot resolve the channel", e);
-		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
-			// TODO log here
-			throw new EventClientException("Invalid topic name", e);
-		}
-		return ChannelAdminHelper.narrow(channelObj);
+    public ORB getOrb() {
+	return orb;
+    }
+
+    /**
+     * Resolve a Channel with its topic using the NameService declared in
+     * configuration.
+     * 
+     * @param topic
+     *            Channel to find topic
+     * @return an instance of Channel (distant object)
+     * @throws EventClientException
+     *             when name resolution is wrong
+     * @throws TopicNotFoundException
+     */
+    public ChannelAdmin resolveChannelByTopic(String topic)
+	    throws EventClientException, TopicNotFoundException {
+	Object channelObj = null;
+	try {
+	    channelObj = nce.resolve_str(topic);
+	    // TODO Design a fail-over to switch between several NameService
+	    // before throwing exception
+	} catch (NotFound e) {
+	    // TODO log here
+	    throw new TopicNotFoundException("Cannot find the Topic '" + topic
+		    + "'", e);
+	} catch (CannotProceed e) {
+	    // TODO log here
+	    throw new EventClientException("Cannot resolve the channel", e);
+	} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+	    // TODO log here
+	    throw new EventClientException("Invalid topic name", e);
 	}
+	return ChannelAdminHelper.narrow(channelObj);
+    }
 
-	public EventServerChannelAdmin resolveEventServerChannelAdminByEventServerName(String eventServerName)
-	{
-		Object channelObj = null;
-		try {
-			channelObj = nce.resolve_str(eventServerName);
-			// TODO Design a fail-over to switch between several NameService
-			// before throwing exception
-		} catch (NotFound e) {
-			// TODO log here
+    public EventServerChannelAdmin resolveEventServerChannelAdminByEventServerName(
+	    String eventServerName) {
+	Object channelObj = null;
+	try {
+	    channelObj = nce.resolve_str(eventServerName);
+	    // TODO Design a fail-over to switch between several NameService
+	    // before throwing exception
+	} catch (NotFound e) {
+	    // TODO log here
 
-		} catch (CannotProceed e) {
-			// TODO log here
+	} catch (CannotProceed e) {
+	    // TODO log here
 
-		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
-			// TODO log here
+	} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+	    // TODO log here
 
-		}
-		return EventServerChannelAdminHelper.narrow(channelObj);
 	}
+	return EventServerChannelAdminHelper.narrow(channelObj);
+    }
 
-
-	/**
-	 * Server a CallBackConsumer and return its IOR.
-	 * 
-	 * @param callbackConsumerImplementation
-	 *            implementation of callbackConsumerOperation
-	 * @return IOR CallbackConsumer
-	 * @throws EventClientException
-	 */
-	public CallbackConsumer serveCallbackConsumer(
-			CallbackConsumerOperations callbackConsumerImplementation)
-	throws EventClientException {
-		Object rootPOAObj = null;
-		try {
-			rootPOAObj = orb.resolve_initial_references("RootPOA");
-		} catch (InvalidName e) {
-			// TODO log here
-			throw new EventClientException("Cannot resolve RootPOA", e);
-		}
-		// TODO make a child POA to handle callbacks
-		callbacksPOA = POAHelper.narrow(rootPOAObj);
-		try {
-			callbacksPOA.the_POAManager().activate();
-		} catch (AdapterInactive e) {
-			throw new EventClientException(
-					"Cannot activate the RootPOAManager", e);
-		}
-		// create a tie, with servant being the delegate.
-		CallbackConsumerPOATie tie = new CallbackConsumerPOATie(
-				callbackConsumerImplementation, callbacksPOA);
-
-		// obtain the objectRef for the tie
-		CallbackConsumer href = tie._this(orb);
-		// return the object and
-		return href;
+    /**
+     * Server a CallBackConsumer and return its IOR.
+     * 
+     * @param callbackConsumerImplementation
+     *            implementation of callbackConsumerOperation
+     * @return IOR CallbackConsumer
+     * @throws EventClientException
+     */
+    public CallbackConsumer serveCallbackConsumer(
+	    CallbackConsumerOperations callbackConsumerImplementation)
+	    throws EventClientException {
+	Object rootPOAObj = null;
+	try {
+	    rootPOAObj = orb.resolve_initial_references("RootPOA");
+	} catch (InvalidName e) {
+	    // TODO log here
+	    throw new EventClientException("Cannot resolve RootPOA", e);
 	}
+	// TODO make a child POA to handle callbacks
+	callbacksPOA = POAHelper.narrow(rootPOAObj);
+	try {
+	    callbacksPOA.the_POAManager().activate();
+	} catch (AdapterInactive e) {
+	    throw new EventClientException(
+		    "Cannot activate the RootPOAManager", e);
+	}
+	// create a tie, with servant being the delegate.
+	CallbackConsumerPOATie tie = new CallbackConsumerPOATie(
+		callbackConsumerImplementation, callbacksPOA);
+
+	// obtain the objectRef for the tie
+	CallbackConsumer href = tie._this(orb);
+	// return the object and
+	return href;
+    }
 
 }
