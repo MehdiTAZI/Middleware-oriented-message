787ea2eee00cd199edb865123bb259db5b6248f7
mail.fabrice.deschamps@gmail.com <mail.fabrice.deschamps@gmail.com@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-27 21:08:54 +0000

    comments for time service

--- mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/ctr/ThreadTime.java
+++ mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/ctr/ThreadTime.java
@@ -8,28 +8,58 @@ import javax.print.attribute.standard.DateTimeAtCompleted;
 import fr.esiag.mezzodijava.mezzo.costime.Synchronizable;
 import fr.esiag.mezzodijava.mezzo.costimeserver.model.TimeServiceModel;
 
+/**
+ * ThreadTime : thread to give time to consumers
+ * 
+ * UC n°06: US18 (+ children)
+ * 
+ * @author Mezzo-Team
+ * 
+ */
 
 public class ThreadTime implements Runnable{
     private TimeServiceModel model;
     private long timeSpan = 1000;
 
-
+    /**
+     * Constructor for a ThreadTime
+     *
+     * @param model
+     *            TimeServiceModel
+     */
 	public ThreadTime(TimeServiceModel model) {
 	this.model=model;
     }
 	
+	/**
+     * Constructor for a ThreadTime
+     *
+     * @param model
+     *            TimeServiceModel
+     * @param timeSpan
+     * 			  
+     */
     public ThreadTime(TimeServiceModel model, long timeSpan) {
 		super();
 		this.model = model;
 		this.timeSpan = timeSpan;
 	}
 
+    /**
+     * To fill date and time to a synchronize component
+     * 			  
+     */
 	public void synchronizeComponent() {
 	for (Synchronizable component : model.getComponentSubscribed()) {
 		Date date=new Date();
 		component.date(date.getTime());
 	 }
     }
+	
+	/**
+     * Run the thread to synchronize components
+     *
+     */
     @Override
     public void run() {
 	while (true) {
@@ -42,10 +72,23 @@ public class ThreadTime implements Runnable{
 	}
     }
     
+    /**
+	 * Get the timespan
+	 *
+	 * @return a timeSpan
+	 *
+	 */
     public long getTimeSpan() {
 		return timeSpan;
 	}
 
+    /**
+	 * Set the timespan
+	 *
+	 * @param timeSpan
+	 * 			a timespan to give time
+	 *
+	 */
 	public void setTimeSpan(long timeSpan) {
 		this.timeSpan = timeSpan;
 	}
--- mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/impl/TimeServiceImpl.java
+++ mezzo-parent/mezzo-costime-server/src/main/java/fr/esiag/mezzodijava/mezzo/costimeserver/impl/TimeServiceImpl.java
@@ -8,19 +8,66 @@ import fr.esiag.mezzodijava.mezzo.costime.TimeServiceOperations;
 import fr.esiag.mezzodijava.mezzo.costime.UnreachableException;
 import fr.esiag.mezzodijava.mezzo.costimeserver.ctr.TimeServiceCtr;
 
+/**
+ * TimeServiceImpl : implementation of the TimeService IDL Interface
+ * 
+ * UC n°06: US18 (+ children)
+ * 
+ * @author Mezzo-Team
+ * 
+ */
+
 public class TimeServiceImpl implements TimeServiceOperations{
 
 	private TimeServiceCtr ctr;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param ctr
+	 *            the TimeServiceCtr
+	 */
 	public TimeServiceImpl(TimeServiceCtr ctr){
 		this.ctr=ctr;
 	}
+	
+	/**
+	 * Get the controller
+	 *
+	 * @return TimeServiceCtr
+	 *
+	 */
 	public TimeServiceCtr getCtr() {
 		return ctr;
 	}
+	
+	/**
+     * Subscribe a component to the channel.
+     * 
+     * The server will push the time to this component.
+     * 
+     * @param component
+     * 			a component of type Synchronizable
+     * @throws AlreadyRegisteredException
+     *             If already present in the list.
+     * @see fr.esiag.mezzodijava.mezzo.costime.TimeServiceOperations#subscribe(Synchronizable cc)
+     */
 	@Override
 	public void subscribe(Synchronizable cc) throws AlreadyRegisteredException{
 		ctr.subscribe(cc);
 	}
+	
+	/**
+     * Unsubscribe a component from the channel.
+     * 
+     * remove the component from the list 
+     * 
+     * @param component
+     * 			a component of type Synchronizable
+     * @throws NotRegisteredException
+     *             If the component is not registered.
+     * @see fr.esiag.mezzodijava.mezzo.costime.TimeServiceOperations#unsubscribe(Synchronizable cc)
+     */
 	@Override
 	public void unsubscribe(Synchronizable cc) throws NotRegisteredException {
 		ctr.unsubscribe(cc);
