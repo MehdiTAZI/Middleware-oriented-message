1424abb3ea7fddbb9cc616c6845a3a69df3850da
tazihosniomar <tazihosniomar@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-02-28 00:35:40 +0000

    MEZZO

---
+++ trunk/mezzo-parent/mezzo-costime-server/src/test/java/fr/esiag/mezzodijava/mezzo/costimeserver/test/TestTimeService.java
@@ -0,0 +1,54 @@
+package fr.esiag.mezzodijava.mezzo.costimeserver.test;
+
+
+import org.easymock.EasyMock;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import fr.esiag.mezzodijava.mezzo.costime.AlreadyRegisteredException;
+import fr.esiag.mezzodijava.mezzo.costime.NotRegisteredException;
+import fr.esiag.mezzodijava.mezzo.costime.Synchronizable;
+import fr.esiag.mezzodijava.mezzo.costimeserver.ctr.TimeServiceCtr;
+import fr.esiag.mezzodijava.mezzo.costimeserver.impl.TimeServiceImpl;
+import fr.esiag.mezzodijava.mezzo.costimeserver.model.TimeServiceModel;
+
+
+public class TestTimeService {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void testSubscrib() throws AlreadyRegisteredException{
+		TimeServiceCtr ctr=new TimeServiceCtr(new TimeServiceModel());
+		TimeServiceImpl test=new TimeServiceImpl(ctr);
+		Synchronizable mockSync=EasyMock.createNiceMock(Synchronizable.class);
+		test.subscribe(mockSync);
+		assertEquals(mockSync,test.getCtr().getModel().getComponentSubscribed().get(0));
+	}
+	@Test
+	public void testUnsubscrib() throws NotRegisteredException, AlreadyRegisteredException{
+		TimeServiceCtr ctr=new TimeServiceCtr(new TimeServiceModel());
+		TimeServiceImpl test=new TimeServiceImpl(ctr);
+		Synchronizable mockSync=EasyMock.createNiceMock(Synchronizable.class);
+		test.subscribe(mockSync);
+		assertTrue(test.getCtr().getModel().getComponentSubscribed().contains(mockSync));
+		test.unsubscribe(mockSync);
+		assertFalse(test.getCtr().getModel().getComponentSubscribed().contains(mockSync));
+	}
+}
