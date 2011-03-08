d77100c14803d765ce7e27d7624bc3bcd2125b59
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-03-08 22:05:52 +0000

    api doc

--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/publisher/ChannelPublisher.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/publisher/ChannelPublisher.java
@@ -16,13 +16,23 @@ import fr.esiag.mezzodijava.mezzo.coseventserver.ctr.ThreadEvent;
 import fr.esiag.mezzodijava.mezzo.coseventserver.factory.BFFactory;
 import fr.esiag.mezzodijava.mezzo.coseventserver.impl.ChannelAdminImpl;
 
+/**
+ * Classe ChannelPublisher
+ * 
+ * To publish or remove the Channel Admin in the ORB
+ * 
+ * UC n°: US14,US15 (+US children)
+ * 
+ * @author Mezzo-Team
+ * 
+ */
 public class ChannelPublisher {
     private static Thread thread;
 
     private static Map<ChannelAdminImpl, byte[]> oidMap = Collections
 	    .synchronizedMap(new HashMap<ChannelAdminImpl, byte[]>());
 
-    public static POA poa;
+    private static POA poa;
 
     private static synchronized POA getPOA() {
 	try {
@@ -42,6 +52,13 @@ public class ChannelPublisher {
 	return poa;
     }
 
+    /**
+     * Publish a ChannelAdminService on the ORB and register it on the Name
+     * Service with the Channel Name (Topic).
+     * 
+     * @param channelAdminImpl
+     *            the implementation of ChannelAdmin
+     */
     public static void publish(ChannelAdminImpl channelAdminImpl) {
 
 	ThreadEvent te = new ThreadEvent(channelAdminImpl.getTopic());
@@ -50,11 +67,11 @@ public class ChannelPublisher {
 	try {
 	    NamingContextExt nc = NamingContextExtHelper.narrow(BFFactory
 		    .getOrb().resolve_initial_references("NameService"));
-	    byte[] oid = getPOA().servant_to_id(new ChannelAdminPOATie(
-		    channelAdminImpl));
+	    byte[] oid = getPOA().servant_to_id(
+		    new ChannelAdminPOATie(channelAdminImpl));
 	    oidMap.put(channelAdminImpl, oid);
-	    nc.rebind(nc.to_name(channelAdminImpl.getTopic()),
-		    getPOA().id_to_reference(oid));
+	    nc.rebind(nc.to_name(channelAdminImpl.getTopic()), getPOA()
+		    .id_to_reference(oid));
 
 	} catch (Exception e) {
 	    // TODO Auto-generated catch block
@@ -62,12 +79,18 @@ public class ChannelPublisher {
 	}
     }
 
+    /**
+     * Deactivate from the POA the ChannelAdmin and unbind it form the name
+     * service.
+     * 
+     * @param channelAdminImpl
+     */
     public static void destroy(ChannelAdminImpl channelAdminImpl) {
 	try {
-	    //corba object deactivation
+	    // corba object deactivation
 	    byte[] oid = oidMap.get(channelAdminImpl);
 	    getPOA().deactivate_object(oid);
-	    //unbind
+	    // unbind
 	    NamingContextExt nc = NamingContextExtHelper.narrow(BFFactory
 		    .getOrb().resolve_initial_references("NameService"));
 	    nc.unbind(nc.to_name(channelAdminImpl.getTopic()));
