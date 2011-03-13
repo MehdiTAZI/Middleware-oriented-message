5ecadad38a5b8ae94833ce194f923f367ee61783
gtmehdi <gtmehdi@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-03-13 01:36:42 +0000

    Application MezzoManager

---
+++ mezzo-manager/mezzo-manager/src/test/java/fr/esiag/mezzodijava/mezzo/manager/client/GwtTestSample.java
@@ -0,0 +1,23 @@
+package fr.esiag.mezzodijava.mezzo.manager.client;
+
+import junit.framework.Assert;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class GwtTestSample
+    extends GWTTestCase
+{
+
+   public String getModuleName()
+   {
+      return "fr.esiag.mezzodijava.mezzo.manager.Application";
+   }
+
+   public void testSomething()
+   {
+      // Not much to actually test in this sample app
+      // Ideally you would test your Controller here (NOT YOUR UI)
+      // (Make calls to RPC services, test client side model objects, test client side logic, etc)
+      Assert.assertTrue( true );
+   }
+}
