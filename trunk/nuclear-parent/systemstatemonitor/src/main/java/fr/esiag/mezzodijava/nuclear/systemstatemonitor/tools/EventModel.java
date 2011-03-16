c301fb04dd4be725ef37d0f24b6952409df7bd47
khalil.elchehab@gmail.com <khalil.elchehab@gmail.com@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-03-16 21:28:08 +0000

    EventModel

---
+++ trunk/nuclear-parent/systemstatemonitor/src/main/java/fr/esiag/mezzodijava/nuclear/systemstatemonitor/tools/EventModel.java
@@ -0,0 +1,106 @@
+package fr.esiag.mezzodijava.nuclear.systemstatemonitor.tools;
+
+import java.io.*;
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+@Entity (name = "EventModel")
+@Table(name = "EventModel")
+public class EventModel implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@javax.persistence.Id
+	@GeneratedValue
+	private int Id;
+	
+	@Column(name = "type")
+	private String type;
+	
+	@Column(name = "code")
+	private long code;
+	
+	@Column(name = "priority")
+	private int priority;
+	
+	@Column(name= "date")
+	private long date;
+	
+	@Column(name = "data")
+	private String data;
+
+	
+	public EventModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EventModel(int id, String type, long code, int priority, long date,
+			String data) {
+		super();
+		Id = id;
+		this.type = type;
+		this.code = code;
+		this.priority = priority;
+		this.date = date;
+		this.data = data;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public long getCode() {
+		return code;
+	}
+
+	public void setCode(long code) {
+		this.code = code;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public long getDate() {
+		return date;
+	}
+
+	public void setDate(long date) {
+		this.date = date;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+}
