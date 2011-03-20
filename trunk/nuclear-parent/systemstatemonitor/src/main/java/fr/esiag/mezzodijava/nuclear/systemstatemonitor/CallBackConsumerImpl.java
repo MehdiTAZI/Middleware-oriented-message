e013ceb90d86c25588264536869754c618c2f8e5
reda.aji@gmail.com <reda.aji@gmail.com@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-03-20 16:18:35 +0000

    change for implementation message de tout type

--- trunk/nuclear-parent/systemstatemonitor/src/main/java/fr/esiag/mezzodijava/nuclear/systemstatemonitor/CallBackConsumerImpl.java
+++ trunk/nuclear-parent/systemstatemonitor/src/main/java/fr/esiag/mezzodijava/nuclear/systemstatemonitor/CallBackConsumerImpl.java
@@ -8,6 +8,9 @@ import fr.esiag.mezzodijava.mezzo.cosevent.Event;
 import fr.esiag.mezzodijava.nuclear.systemmonitor.DB.DbEventConnector;
 import fr.esiag.mezzodijava.nuclear.systemmonitor.DB.DbEventConnectorImpl;
 import fr.esiag.mezzodijava.nuclear.systemstatemonitor.tools.EventInfo;
+import fr.esiag.nuclear.commons.model.Pression;
+import fr.esiag.nuclear.commons.model.RadioActivite;
+import fr.esiag.nuclear.commons.model.Temperature;
 
 public class CallBackConsumerImpl implements CallbackConsumerOperations {
 
@@ -27,10 +30,37 @@ public class CallBackConsumerImpl implements CallbackConsumerOperations {
 
     @Override
     public void receive(Event e) throws ConsumerNotFoundException {
-
-	System.out.println("Message recu timestamp:" + e.header.timetolive
-		+ ", contenu " + e.body.content);
-
+    	    	
+    if(e.body.type.equals("String")){
+    	System.out.println("IN STRING");
+	System.out.println("Message recu type=(String) timestamp:" + e.header.timetolive
+		+ ", contenu " + e.body.content.extract_string());
+    }
+    
+    if(e.body.type.equals("Temperature")){
+    	System.out.println("IN TEMERATURE");
+		Temperature t=(Temperature)e.body.content.extract_Value();
+		System.out.println("Message recu type=(Temperature) timestamp:" + e.header.timetolive
+				+ ", contenu: Temperature" + " Value: "+t.getValue() +"| Unite: "+t.getUnite());
+			
+	}
+    
+    if(e.body.type.equals("Pression")){
+    	System.out.println("IN Pression");
+		Pression t=(Pression)e.body.content.extract_Value();
+		System.out.println("Message recu type=(Pression) timestamp:" + e.header.timetolive
+				+ ", contenu: Pression" + " Value: "+t.getValue() +"| Unite: "+t.getUnite());
+			
+	}
+    
+    if(e.body.type.equals("RadioActivite")){
+    	System.out.println("IN RADIOACTIVITE");
+		RadioActivite t=(RadioActivite)e.body.content.extract_Value();
+		System.out.println("Message recu type=(RadioActivite) timestamp:" + e.header.timetolive
+				+ ", contenu: RadioActivite" + " Value: "+t.getValue() +"| Unite: "+t.getUnite());
+			
+	}
+    
 	EventInfo eventInfo = new EventInfo(e);
 	int sum = 0;
 	int average = 0;
@@ -59,6 +89,7 @@ public class CallBackConsumerImpl implements CallbackConsumerOperations {
 	// if the state has changed over 10% since last 5 events,
 	// alert! code 999
 	// state change event.header.priority = 2
+	System.out.println("DATA"+eventInfo.getData());
 	boolean state = (average - Integer.valueOf(eventInfo.getData())) > (average * 10 / 100);
 	if (state) {
 	    e.header.code = 999;
--- trunk/nuclear-parent/systemstatemonitor/src/main/java/fr/esiag/mezzodijava/nuclear/systemstatemonitor/InjectorSystemStateSupplier.java
+++ trunk/nuclear-parent/systemstatemonitor/src/main/java/fr/esiag/mezzodijava/nuclear/systemstatemonitor/InjectorSystemStateSupplier.java
@@ -11,6 +11,9 @@ import fr.esiag.mezzodijava.mezzo.cosevent.ProxyForPushSupplier;
 import fr.esiag.mezzodijava.mezzo.libclient.EventClient;
 import fr.esiag.mezzodijava.mezzo.libclient.exception.EventClientException;
 import fr.esiag.mezzodijava.mezzo.libclient.exception.TopicNotFoundException;
+import fr.esiag.nuclear.commons.model.Pression;
+import fr.esiag.nuclear.commons.model.RadioActivite;
+import fr.esiag.nuclear.commons.model.Temperature;
 
 public class InjectorSystemStateSupplier {
 	
@@ -44,8 +47,36 @@ public class InjectorSystemStateSupplier {
 	{
 		try {
 			supplierProxy.push(e);
+			
+			if(e.body.type.equals("String")){	
+				System.out.println("IN STRING");
 			System.out.println("!!ALERTE envoyee : Code("+e.header.code+"), Timestamp:" + e.header.creationdate
-					+ ", contenu " + e.body.content);
+					+ ", contenu " + e.body.content.extract_string());
+			}
+			if(e.body.type.equals("Temperature")){
+				System.out.println("PUSH EVENT");
+				Temperature t=(Temperature)e.body.content.extract_Value();
+				System.out.println("!!ALERTE envoyee : Code("+e.header.code+"), Timestamp:" + e.header.creationdate
+						+ ", contenu " + t.getValue() +"/"+t.getUnite());
+					
+			}
+			
+			  if(e.body.type.equals("Pression")){
+			    	System.out.println("IN Pression");
+					Pression t=(Pression)e.body.content.extract_Value();
+					System.out.println("!!ALERTE envoyee : Code("+e.header.code+"), Timestamp:" + e.header.creationdate
+							+ ", contenu: Pression" + " Value: "+t.getValue() +"| Unite: "+t.getUnite());
+						
+				}
+			    
+			    if(e.body.type.equals("RadioActivite")){
+			    	System.out.println("IN RADIOACTIVITE");
+					RadioActivite t=(RadioActivite)e.body.content.extract_Value();
+					System.out.println("!!ALERTE envoyee : Code("+e.header.code+"), Timestamp:" + e.header.creationdate
+							+ ", contenu: RadioActivite" + " Value: "+t.getValue() +"| Unite: "+t.getUnite());
+						
+				}
+			    
 			
 		} catch (ChannelNotFoundException e1) {
 			// TODO Auto-generated catch block
