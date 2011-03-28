39f2fc04231093d38c33f40258df3afd8018063f
mail.fabrice.deschamps@gmail.com <mail.fabrice.deschamps@gmail.com@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-03-28 13:42:39 +0000

    ajout du dao

---
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/dao/ChannelDAO.java
@@ -0,0 +1,7 @@
+package fr.esiag.mezzodijava.mezzo.coseventserver.dao;
+
+import fr.esiag.mezzodijava.mezzo.coseventserver.model.ChannelModel;
+
+public interface ChannelDAO extends DAO<String, ChannelModel>{
+
+}
