ecb110736f77e3e23c009d8406e39515299b1d81
tazihosniomar <tazihosniomar@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-03 23:01:42 +0000

    MEZZO

--- trunk/mezzo-manager/mezzo-manager/src/main/java/fr/esiag/mezzodijava/mezzo/manager/server/AuthenticationServiceImpl.java
+++ trunk/mezzo-manager/mezzo-manager/src/main/java/fr/esiag/mezzodijava/mezzo/manager/server/AuthenticationServiceImpl.java
@@ -12,11 +12,17 @@ public class AuthenticationServiceImpl  extends RemoteServiceServlet implements 
 	private static final long serialVersionUID = 1L;
 
 	public ChannelInfosCollector[] authenticate(String loging, String pwd) {
-		ManagerClient manager=ManagerClient.init(null);
+		System.out.println(loging+":"+pwd);
+		ManagerClient manager=ManagerClient.init(new String[]{"-ORBInitRef NameService=corbaloc::127.0.0.1:1050/NameService",
+				"-Djacorb.home=C:\\mezzodev\\jacorb-2.3.1",
+				"-Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB",
+				"-Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton"});	
 		CosInfoCollector managerInfo=manager.resolveManagerService("CosInfoCollector");
-		fr.esiag.mezzodijava.mezzo.monitoring.ChannelInfosCollector[] channelInfosCollector=managerInfo.getChannelsInfos();
+		fr.esiag.mezzodijava.mezzo.monitoring.ChannelInfosCollector[] channelInfosCollector=new fr.esiag.mezzodijava.mezzo.monitoring.ChannelInfosCollector[managerInfo.getChannelsInfos().length];
+		channelInfosCollector=managerInfo.getChannelsInfos();
 		ChannelInfosCollector[] channelsInfo=new ChannelInfosCollector[channelInfosCollector.length];
 		int i=0;
+		
 		for(fr.esiag.mezzodijava.mezzo.monitoring.ChannelInfosCollector channelInfo:channelInfosCollector){
 			channelsInfo[i].capacity=channelInfo.capacity;
 			channelsInfo[i].consumersConnected=channelInfo.consumersConnected;
