b0d57e4be88a720faf5c6e2b895a5f861037bdb0
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-06 13:54:16 +0000

    timecallback : plus de sysout

--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/impl/CallbackTimeImpl.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/impl/CallbackTimeImpl.java
@@ -2,28 +2,30 @@ package fr.esiag.mezzodijava.mezzo.coseventserver.impl;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import fr.esiag.mezzodijava.mezzo.coseventserver.main.CosEventServer;
 import fr.esiag.mezzodijava.mezzo.costime.SynchronizableOperations;
 
+public class CallbackTimeImpl implements SynchronizableOperations {
 
-public class CallbackTimeImpl implements SynchronizableOperations{
+    final static Logger log = LoggerFactory.getLogger(CallbackTimeImpl.class);
+    private long delta;
 
-	private long delta;
-	
-	@Override
-	public long date() {
-		
-		return CosEventServer.getDelta();
-	}
+    @Override
+    public long date() {
 
-	@Override
-	public void date(long arg) {
-		
-		delta=arg - new Date().getTime();
-		System.out.println(" Time synchronisé "+delta);
-		CosEventServer.setDelta(delta);
-		
-		
-	}
+	return CosEventServer.getDelta();
+    }
+
+    @Override
+    public void date(long arg) {
+
+	delta = arg - new Date().getTime();
+	log.trace(" Time synchronisé " + delta);
+	CosEventServer.setDelta(delta);
+
+    }
 
 }
