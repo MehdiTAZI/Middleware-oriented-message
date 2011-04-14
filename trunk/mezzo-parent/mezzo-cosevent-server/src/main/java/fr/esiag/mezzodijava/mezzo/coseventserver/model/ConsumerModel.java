e906f05feef6a11eee4fd49ac4881d533e0398ef
zakai2y@gmail.com <zakai2y@gmail.com@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-14 06:25:47 +0000

    COMMENTS MODEL

--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/model/ConsumerModel.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/model/ConsumerModel.java
@@ -19,34 +19,66 @@ public class ConsumerModel {
     private SortedSet<EventModel> eventsInQueue = Collections
 	    .synchronizedSortedSet(new TreeSet<EventModel>(new PriorityEventModelComparator()));
 
+    /**
+     * Getter of the Id
+     * @return
+     */
     public int getId() {
 	return id;
     }
 
+    /**
+     * Set the Id
+     * @param id
+     */
     public void setId(int id) {
 	this.id = id;
     }
 
+    /**
+     * Getter of the IdConsumer
+     * @return
+     */
     public String getIdConsumer() {
 	return idConsumer;
     }
 
+    /**
+     * Set the IdConsumer
+     * @param idConsumer
+     */
     public void setIdConsumer(String idConsumer) {
 	this.idConsumer = idConsumer;
     }
 
+    /**
+     * Getter of the events associated to the consumer
+     * @return
+     */
     public SortedSet<EventModel> getEvents() {
 	return eventsInQueue;
     }
 
+    /**
+     * Set the Events of the Consumer
+     * @param events
+     */
     public void setEvents(SortedSet<EventModel> events) {
 	this.eventsInQueue = events;
     }
 
+    /**
+     * Getter of the channel's consumer
+     * @return
+     */
 	public Channel getChannel() {
 		return channel;
 	}
 
+	/**
+	 * Set the channel associated to the consumer
+	 * @param channel
+	 */
 	public void setChannel(Channel channel) {
 		this.channel = channel;
 	}
--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/model/EventModel.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/model/EventModel.java
@@ -20,10 +20,22 @@ public class EventModel implements Serializable {
 
     private String type;
 
+    /**
+     * Constructor
+     */
     public EventModel() {
 	super();
     }
-
+/**
+ * Contructor
+ * @param id
+ * @param type
+ * @param code
+ * @param priority
+ * @param data
+ * @param creationdate
+ * @param timetolive
+ */
     public EventModel(int id, String type, long code, int priority,
 	    byte[] data, long creationdate, long timetolive) {
 	super();
@@ -36,62 +48,122 @@ public class EventModel implements Serializable {
 	this.type = type;
     }
 
+    /**
+     * Getter of the Id
+     * @return
+     */
     public int getId() {
 	return id;
     }
 
+    /**
+     * Set the Id
+     * @param id
+     */
     public void setId(int id) {
 	this.id = id;
     }
 
+    /**
+     * Getter of the Priority
+     * @return
+     */
     public int getPriority() {
 	return priority;
     }
 
+    /**
+     * Set the Priority
+     * @param priority
+     */
     public void setPriority(int priority) {
 	this.priority = priority;
     }
 
+    /**
+     * Getter of the data
+     * @return
+     */
     public byte[] getData() {
 	return data;
     }
 
+    /**
+     * Set the data
+     * @param data
+     */
     public void setData(byte[] data) {
 	this.data = data.clone();
     }
 
+    /**
+     * Getter of svuid
+     * @return
+     */
     public static long getSerialversionuid() {
 	return serialVersionUID;
     }
 
+    /**
+     * Getter of the code
+     * @return
+     */
     public long getCode() {
 	return code;
     }
 
+    /**
+     * Set the code
+     * @param code
+     */
     public void setCode(long code) {
 	this.code = code;
     }
 
+    /**
+     * Getter of the creationDate
+     * @return
+     */
     public long getCreationdate() {
 	return creationdate;
     }
 
+    /**
+     * Set the creationDate
+     * @param creationdate
+     */
     public void setCreationdate(long creationdate) {
 	this.creationdate = creationdate;
     }
 
+    /**
+     * Getter of the TimeToLive
+     * @return
+     */
     public long getTimetolive() {
 	return timetolive;
     }
 
+    /**
+     * Set the TimeToLive
+     * @param timetolive
+     */
     public void setTimetolive(long timetolive) {
 	this.timetolive = timetolive;
     }
 
+    /**
+     * Getter of the type
+     * @return
+     */
     public String getType() {
 	return type;
     }
 
+    /**
+     * Set the type
+     * @param type
+     */
     public void setType(String type) {
 	this.type = type;
     }
