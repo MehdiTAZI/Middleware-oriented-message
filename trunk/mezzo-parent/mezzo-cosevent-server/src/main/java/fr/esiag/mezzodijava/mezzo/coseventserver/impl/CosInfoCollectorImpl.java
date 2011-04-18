834dd3b4dab8c5d5bc26291b8c2f5cb86291b83a
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-18 22:51:41 +0000

    Rules

--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/impl/CosInfoCollectorImpl.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/impl/CosInfoCollectorImpl.java
@@ -1,12 +1,9 @@
 package fr.esiag.mezzodijava.mezzo.coseventserver.impl;
 
-import java.util.SortedSet;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.esiag.mezzodijava.mezzo.coseventserver.model.Channel;
-import fr.esiag.mezzodijava.mezzo.coseventserver.model.EventModel;
 import fr.esiag.mezzodijava.mezzo.coseventserver.model.EventServer;
 import fr.esiag.mezzodijava.mezzo.monitoring.ChannelInfosCollector;
 import fr.esiag.mezzodijava.mezzo.monitoring.CosInfoCollectorOperations;
@@ -56,25 +53,25 @@ public class CosInfoCollectorImpl implements CosInfoCollectorOperations{
 		}
 		return list;
 	}
-	private Message[] EventsToMessage(SortedSet<EventModel> events){
-		log.debug("Access to EventtoMessage Module");
-		Message [] messages=new Message[events.size()];
-		int i=0;
-		for(EventModel event:events)
-		{
-			messages[i]=new Message();
-			messages[i].code=""+event.getCode();
-			messages[i].time=""+event.getTimetolive();
-			messages[i].type=""+event.getType();
-			messages[i].data=new String(event.getData());
-			log.trace("Message number {}",i);
-			log.trace("Code: "+messages[i].code);
-			log.trace("Time: "+messages[i].time);
-			log.trace("Type: "+messages[i].type);
-			log.trace("Data: "+messages[i].data);
-			log.trace("End of message number {}",i);
-		}
-		return messages;
-	}
+//	private Message[] EventsToMessage(SortedSet<EventModel> events){
+//		log.debug("Access to EventtoMessage Module");
+//		Message [] messages=new Message[events.size()];
+//		int i=0;
+//		for(EventModel event:events)
+//		{
+//			messages[i]=new Message();
+//			messages[i].code=""+event.getCode();
+//			messages[i].time=""+event.getTimetolive();
+//			messages[i].type=""+event.getType();
+//			messages[i].data=new String(event.getData());
+//			log.trace("Message number {}",i);
+//			log.trace("Code: "+messages[i].code);
+//			log.trace("Time: "+messages[i].time);
+//			log.trace("Type: "+messages[i].type);
+//			log.trace("Data: "+messages[i].data);
+//			log.trace("End of message number {}",i);
+//		}
+//		return messages;
+//	}
 
 }
