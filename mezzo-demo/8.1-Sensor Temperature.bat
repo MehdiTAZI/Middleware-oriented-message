0e8c6678ac85910728cca02b4a3bc8571adc6643
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-20 13:54:03 +0000

    lanceurs sensors

---
+++ mezzo-demo/8.1-Sensor Temperature.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE 8.1 - CAPTEURS TEMPERATURE °C
+call java -jar sensor.jar 127.0.0.1 temperature.txt
+pause
---
+++ mezzo-demo/8.3-Sensor Radioactivite.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE 8.3 - CAPTEURS RADIOACTIVITE Bq
+call java -jar sensor.jar 127.0.0.1 radioactivite.txt
+pause
