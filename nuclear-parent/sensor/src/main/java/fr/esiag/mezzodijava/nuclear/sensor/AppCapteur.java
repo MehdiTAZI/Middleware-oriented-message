5e11e167dbc9deba16dc2c43805bca25dffa0da6
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-20 13:58:42 +0000

    capteur en multi thread

--- nuclear-parent/sensor/src/main/java/fr/esiag/mezzodijava/nuclear/sensor/AppCapteur.java
+++ nuclear-parent/sensor/src/main/java/fr/esiag/mezzodijava/nuclear/sensor/AppCapteur.java
@@ -6,41 +6,46 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
-/*
+/**
  *  Mettre en program arguments :
- *  pression.txt radioactivite.txt temperature.txt
+ *  <adresse IP> pression.txt radioactivite.txt temperature.txt
+ *  
+ *  ex :
+ *  
+ *  127.0.0.1 pression.txt
  */
-public class AppCapteur 
-{
-	public static void main( String[] args ) throws UnknownHostException, IOException
-	{
-		
-		Socket socket = new Socket("127.0.0.1",9191);
-		PrintWriter writer = new PrintWriter(
-				new BufferedWriter(
-						new OutputStreamWriter(socket.getOutputStream())),
-						true);
-		Capteur capteur=new Capteur(new TextFile());
-		Vector<String> data = new Vector<String>();
-		for (String arg : args){
-			System.out.println("CHARGEMENT DE : "+ arg);
-			data.addAll(capteur.getData(arg));
-		}
-		for(String message:data){
-			System.out.println(message);
-			writer.println(message);//envoie de Message au Sensorsdatasupplier     
-			try {
-			    Thread.sleep(2000);
-			} catch (InterruptedException e) {
-			    e.printStackTrace();
-			}
-		}
-		System.out.println("END");     // message de terminaison 
-		writer.println("END") ;
-		writer.close();
-		socket.close();
+public class AppCapteur {
+    public static void main(String[] args) throws UnknownHostException,
+	    IOException {
+
+	Socket socket = new Socket(args[0], 9191);
+	for (int i = 1; i < args.length; i++) {
+
 	}
-	
+	PrintWriter writer = new PrintWriter(new BufferedWriter(
+		new OutputStreamWriter(socket.getOutputStream())), true);
+	Capteur capteur = new Capteur(new TextFile());
+	List<String> data = new ArrayList<String>();
+	for (int i = 1; i < args.length; i++) {
+	    System.out.println("CHARGEMENT DE : " + args[i]);
+	    data.addAll(capteur.getData(args[i]));
+	}
+	for (String message : data) {
+	    System.out.println(message);
+	    writer.println(message);// envoie de Message au Sensorsdatasupplier
+	    try {
+		Thread.sleep(2000);
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+	System.out.println("END"); // message de terminaison
+	writer.println("END");
+	writer.close();
+	socket.close();
+    }
+
 }
