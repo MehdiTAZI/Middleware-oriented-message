10d3169ace65853da91640bd514d5b521b26065d
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-22 10:31:55 +0000

    dbfile property

--- trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/dao/DAOFactory.java
+++ trunk/mezzo-parent/mezzo-cosevent-server/src/main/java/fr/esiag/mezzodijava/mezzo/coseventserver/dao/DAOFactory.java
@@ -1,28 +1,36 @@
 package fr.esiag.mezzodijava.mezzo.coseventserver.dao;
 
+import fr.esiag.mezzodijava.mezzo.coseventserver.main.CosEventServer;
 import fr.esiag.mezzodijava.mezzo.coseventserver.model.EventServer;
 
 /**
  * 
- * @author MEZZODIJAVA
- * Abstract factory
- *
+ * @author MEZZODIJAVA Abstract factory
+ * 
  */
 public abstract class DAOFactory {
 
     private static JdbcDAO jdbcDAOinstance = null;
 
-    private DAOFactory(){
-	
+    private DAOFactory() {
+
     }
-    
+
     /**
      * Singleton access to the JdbcDAO
+     * 
      * @return the jdbcDAO
      */
     public static synchronized JdbcDAO getJdbcDAO() {
 	if (jdbcDAOinstance == null) {
-		jdbcDAOinstance = new JdbcDOAImpl("test","test",System.getProperty("user.home")+System.getProperty("file.separator")+"dbcosevent"+EventServer.getInstance().getServerName());
+	    String dbFile = CosEventServer.properties
+		    .getProperty("eventserver.persistence.dbfile");
+	    if (dbFile == null) {
+		dbFile = System.getProperty("user.home")
+			+ System.getProperty("file.separator") + "dbcosevent"
+			+ EventServer.getInstance().getServerName();
+	    }
+	    jdbcDAOinstance = new JdbcDOAImpl("test", "test", dbFile);
 	}
 	return jdbcDAOinstance;
     }
