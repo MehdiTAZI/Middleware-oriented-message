e09e59f9bda7d210305a08be5c2fc3f815b2cf45
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-26 23:27:59 +0000

    mezzo-demo en log4j

--- trunk/mezzo-demo/4-COSEventServer-ARGENTINA.bat
+++ trunk/mezzo-demo/4-COSEventServer-ARGENTINA.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE 4 - COSEvent Server ARGENTINA
-call jaco -Djava.util.logging.config.file=c:\mezzodev\mezzo-demo\eventserver.properties -Djacorb.home=C:\mezzodev\jacorb-2.3.1 -classpath "c:\mezzodev\mezzo-demo;c:\mezzodev\mezzo-demo;mezzo-cosevent-server.jar" fr.esiag.mezzodijava.mezzo.coseventserver.main.CosEventServer ARGENTINA_SERVER
+call jaco -Dlog4j.configuration=eventargentinalog4j.properties -Djacorb.home=C:\mezzodev\jacorb-2.3.1 -classpath "c:\mezzodev\mezzo-demo;c:\mezzodev\mezzo-demo;mezzo-cosevent-server.jar" fr.esiag.mezzodijava.mezzo.coseventserver.main.CosEventServer ARGENTINA_SERVER
 pause
