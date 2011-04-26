fcef80967476f3df883e371a08f2a5b7069094dc
franck.gineste <franck.gineste@20500de7-2642-f3e1-8dcf-a484bc037fc5>
2011-04-26 23:27:59 +0000

    mezzo-demo en log4j

--- mezzo-demo/3a-COSTime-MEZZO-COSTIME.bat
+++ mezzo-demo/3a-COSTime-MEZZO-COSTIME.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE 3a - COSTime MEZZO COSTIME
-call jaco -Djava.util.logging.config.file=c:\mezzodev\mezzo-demo\timeserver.properties -Djacorb.home=C:\mezzodev\jacorb-2.3.1 -classpath "c:\mezzodev\mezzo-demo;c:\mezzodev\mezzo-demo\mezzo-costime-server.jar" fr.esiag.mezzodijava.mezzo.costimeserver.main.CosTimeServer MEZZO-COSTIME
+call jaco -Dlog4j.configuration=timelog4j.properties -Djacorb.home=C:\mezzodev\jacorb-2.3.1 -classpath "c:\mezzodev\mezzo-demo;c:\mezzodev\mezzo-demo\mezzo-costime-server.jar" fr.esiag.mezzodijava.mezzo.costimeserver.main.CosTimeServer MEZZO-COSTIME
 pause
